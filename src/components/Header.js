75dc195ed852d5c3aa7190bf9063b2a2563686d9
hadeellafi <hadeellafi9@gmail.com>
2023-10-07 18:47:21 +0000

    lab37

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <nav className="py-4 bg-red-900">
+        <h1 className="pl-2 text-2xl text-red-100">Cookie Stand Admin</h1>
+      </nav>
+    )
+    }
+    export default Header;
+    
